131030985001881093d98ac7382416d0b4bf3719
lietxia <lietxia@gmail.com>
2020-02-18 11:22:24 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 echo "keepalive..."
 touch /tmp/keepalive
 
@@ -5,13 +6,12 @@ docker rmi `docker images -q`
 echo "Deleting files, please wait ..."
 
 sudo rm -rf /usr/share/dotnet /etc/mysql /etc/php /etc/apt/sources.list.d
-sudo -E apt-get -y purge azure-cli ghc* zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php*
+sudo -E apt-get -y purge azure-cli ghc* zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* php*
 
 echo "apt install"
-sudo -E apt-get update
-sudo -E apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler
-sudo -E apt-get -y autoremove --purge
-sudo -E apt-get clean
+sudo -E apt update
+sudo -E apt -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler
+sudo -E apt -y autoremove --purge
 sudo ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 
